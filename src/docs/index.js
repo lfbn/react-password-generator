316f5819b58faad6bc8c4f9027db2198b9d10c8f
Luis Nobrega <luis.nobrega@byside.com>
2017-10-25 23:42:20 +0100

    Removed not used constructor.

--- src/docs/index.js
+++ src/docs/index.js
@@ -9,16 +9,43 @@ import '../../style.css'
 import './style.css'
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-  }
-
   render () {
+    const messages = {
+      inputPlaceholder: 'This is a custom input placeholder',
+      generateButtonLabel: 'This a custom button label',
+    }
+
     return (
       <form>
         <h1>react-input-password-generator</h1>
+        <h2>Simple Example</h2>
         <div className='example'>
           <InputPasswordGenerator />
+        </div>
+        <hr />
+        <h2>Example with a custom length password</h2>
+        <div className='example'>
+          <InputPasswordGenerator password={{password: {'customLength': 15}}} />
+        </div>
+        <hr />
+        <h2>Example with a not memorable password</h2>
+        <div className='example'>
+          <InputPasswordGenerator password={{password: {'notMemorable': false}}} />
+        </div>
+        <hr />
+        <h2>Example with a custom Class Name</h2>
+        <div className='example'>
+          <InputPasswordGenerator className={'red'} />
+        </div>
+        <hr />
+        <h2>Example with a custom Class Name</h2>
+        <div className='example'>
+          <InputPasswordGenerator className={'red'} />
+        </div>
+        <hr />
+        <h2>Example with a custom button label or input placeholder</h2>
+        <div className='example'>
+          <InputPasswordGenerator messages />
         </div>
       </form>
     )
